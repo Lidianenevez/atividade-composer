64f9309e5eae413723484ba9a34d927540c78d76
Lidianenevez <lidianeneves79@gmail.com>
2018-09-24 21:57:47 -0300

    validando

--- index.php
+++ index.php
@@ -1,14 +1,20 @@
 <html>
-	
-<form action="receber.php" method="post">
+	<form action="receber.php" method="post">
   <div class="form-group">
     <label >Nome</label>
-    <input type="text" name="nome" >
+    <input type="text" name="nome" required >
+    <br>
+    <label >E-mail</label>
+    <input type="email" name="email" required >
+    <br>
+    <label >Data de Nascimento</label>
+    <input type="date" name="date" required>
   </div>
   
   <button type="submit" >Submit</button>
 </form>
 </html>
+
 
 
 
--- receber.php
+++ receber.php
@@ -1,10 +1,23 @@
 <?php 
 require_once 'vendor/autoload.php'; 
 $nome=$_POST['nome'];
-use Endroid\QrCode\QrCode;
+$email=$_POST['email'];
+$date=$_POST['date'];
 
-$qrCode = new QrCode($nome);
+use Respect\Validation\Validator as v;
 
-header('Content-Type: '.$qrCode->getContentType());
-echo $qrCode->writeString() ;?>
-?>
+$evalido=v::email()->validate($email);
+$data=v::date()->validate($date);
+
+if ($evalido === true){
+	echo "$email: <b>Valido</b> <br>";
+
+}else{
+	echo " $email: <b>invalido</b> <br>";
+}
+
+if ($data === true){
+	echo " $date: <b>Valido</b><br>";
+}else{
+	echo " $date: <b>invalido</b> <br>";
+}
